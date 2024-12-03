535a6badc31f9688757cc57a4408bf5671a60834
assafva <assafva.adventofcode@outlook.com>
2024-12-03 12:58:34 +0200

    day1 cleanup

--- AdventOfCode2024/Days/D1.cs
+++ AdventOfCode2024/Days/D1.cs
@@ -45,43 +45,6 @@ public class D1 : AocDay
         return (sumA.ToString(), sumB.ToString());
     }
 
-    private int MicroOptimizedB(IList<int> lhs, IList<int> rhs)
-    {
-        var iteratorLhs = lhs;
-        var iteratorRhs = rhs;
-
-        var lhsPointer = 0;
-        var rhsPointer = 0;
-
-        while (lhsPointer < lhs.Count)
-        {
-            int sameCount = 1;
-            while (lhsPointer < lhs.Count - 1 && lhs[lhsPointer] == lhs[lhsPointer + 1])
-            {
-                lhsPointer++;
-                sameCount++;
-            }
-
-            int instanceCount = 0;
-            while (rhs[rhsPointer] == lhs[lhsPointer] && rhsPointer < rhs.Count)
-            {
-                
-            }
-        }
-
-        return 0;
-    }
-
-    private class DuplicateKeyComparer : IComparer<int>
-    {
-        // I know it's incorrect, it works for this specific use
-        public int Compare(int x, int y)
-        {
-            var comparison = x.CompareTo(y);
-            return comparison != 0 ? comparison : 1;
-        }
-    }
-
     private static IEnumerable<int> FlatMapEnumerate(SortedDictionary<int, int> sortedDict)
     {
         foreach (var (number, instances) in sortedDict)
